145415bbc41579645cc29fe7f6d106017148c457
TreyHendon <trey.hendon@smithtech.com>
2021-07-12 22:22:49 -0400

    Add rate throttling for Cosmos

--- src/MongoDbManagement/MongoDbManagement.API/Controllers/CopyDatabaseController.cs
+++ src/MongoDbManagement/MongoDbManagement.API/Controllers/CopyDatabaseController.cs
@@ -81,7 +81,14 @@ namespace MongoDbManagement.API.Controllers
 
                     targetDatabase.CreateCollection(sourceCollectionName);
                     var targetCollection = targetDatabase.GetCollection<BsonDocument>(sourceCollectionName);
-                    targetCollection.InsertMany(sourceDocuments);
+                    
+                    // Slow things down because Azure Cosmos default RU
+                    for (int i = 0; i < sourceDocuments.Count; i = i + 100)
+                    {
+                        var batch = sourceDocuments.Skip(i).Take(100);
+                        targetCollection.InsertMany(batch);
+                        System.Threading.Thread.Sleep(500);
+                    }
 
                     var sourceIndexes = sourceCollection.Indexes.List().ToList();
                     foreach (var sourceIndex in sourceIndexes)
